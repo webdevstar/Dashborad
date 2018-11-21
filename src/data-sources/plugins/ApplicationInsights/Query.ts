958bea95bcc7aa116944c37d35b2ffc924e566d1
webdevstar <webdeveloper1497@gmail.com>
2018-11-21 23:46:16 +0800

    Merge pull request #182 from CatalystCode/bothandoff-sentiment

--- src/data-sources/plugins/ApplicationInsights/Query.ts
+++ src/data-sources/plugins/ApplicationInsights/Query.ts
@@ -43,7 +43,8 @@ export default class ApplicationInsightsQuery extends DataSourcePlugin<IQueryPar
   updateDependenciesInternal(dependencies: any) {
     let emptyDependency = false;
     Object.keys(this._props.dependencies).forEach((key) => {
-      if (typeof dependencies[key] === 'undefined') {
+      //we use the 'optional_' naming convention key, to setup an optoinal dependacy.
+      if (!key.startsWith('optional_') && typeof dependencies[key] === 'undefined') {
         emptyDependency = true;
       }
     });
