dbedcc15ed4e4ce445c711b5e8a1c0ce7bfc2a2d
webdevstar <webdeveloper1497@gmail.com>
2018-12-15 22:48:26 +0800

    removed test Toast warning

--- client/src/components/Home/index.tsx
+++ client/src/components/Home/index.tsx
@@ -16,7 +16,6 @@ import ConfigurationStore from '../../stores/ConfigurationsStore';
 import ConfigurationsActions from '../../actions/ConfigurationsActions';
 import utils from '../../utils';
 
-import IconPicker from './IconPicker';
 import { downloadBlob } from '../Dashboard/DownloadFile';
 
 const renderHTML = require('react-render-html');
@@ -188,7 +187,7 @@ export default class Home extends React.Component<any, IHomeState> {
     let createParams = {
       id: this._fieldId.getField().value,
       name: this._fieldName.getField().value,
-      icon: this._fieldIcon.getIcon(),
+      icon: this._fieldIcon.getField().value,
       url: this._fieldId.getField().value
     };
 
@@ -420,10 +419,6 @@ export default class Home extends React.Component<any, IHomeState> {
             { onClick: this.onNewTemplateSave, primary: true, label: 'Create', },
           ]}
         >
-          <IconPicker 
-            ref={field => this._fieldIcon = field}
-            defaultLabel="Dashboard Icon" 
-            defaultIcon={template && template.icon || 'dashboard'} />
           <TextField
             id="id"
             ref={field => this._fieldId = field}
@@ -441,6 +436,14 @@ export default class Home extends React.Component<any, IHomeState> {
             defaultValue={template && template.name || ''}
             lineDirection="center"
             placeholder="Choose name for the dashboard (will be used in navigation)"
+          />
+          <TextField
+            id="icon"
+            ref={field => this._fieldIcon = field}
+            label="Dashboard Icon"
+            defaultValue={template && template.icon || 'dashboard'}
+            lineDirection="center"
+            placeholder="Choose icon for the dashboard (will be used in navigation)"
           />
         </Dialog>
       </div>
